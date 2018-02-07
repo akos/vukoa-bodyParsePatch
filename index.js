531c003181dea2e53e3319e38a6a47a664db9ed0
kocolin <apple@lgfshrdeMacBook-Pro.local>
2018-02-08 03:10:00 +0800

    :construction: add multipart deal

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-module.exports=require('./lib/bodyParsePath')
+const processMethod = require('./lib/processMethod');
+const parsePatch = require('./lib/parsePatch');
+
+module.exports = function bodyParsePatch(options) {
+    options = processMethod.defaultSetting(options)
+    let onerror = options.onerror;
+
+    return async function plugin (ctx, next) {
+        let multipart = true;
+        if (options.strict && !processMethod.isValid(this.method)) {
+            return await next
+        }
+
+        if (ctx.request.body !== undefined) return await next();
+        if (ctx.disableBodyParser) return await next();
+        try {
+            processMethod.setParsers(ctx);
+            const res = await processMethod.parseBody(ctx, options);
+            if(res !== 'multipart'){
+                ctx.request.body = 'parsed' in res ? res.parsed : {};
+                multipart = false;
+            }
+            if (ctx.request.rawBody === undefined) ctx.request.rawBody = res.raw;
+            await parsePatch(ctx, multipart);
+        } catch (err) {
+            if (onerror) {
+                onerror(err, ctx);
+            } else {
+                throw err;
+            }
+        }
+        await next();
+    }
+}
---
+++ test/exampleTest.js
@@ -0,0 +1,46 @@
+// const Koa = require('koa');
+// const app = new Koa();
+// const bodyParsePatch = require('../index');
+// app.use(bodyParsePatch({
+//     multipart: true,
+//     keepExtensions: true,
+//     strict: false,
+// }));
+// app.use(async ctx => {
+//     ctx.body = 'Hello World';
+// });
+//
+// app.listen(3000);
+const Koa = require('koa');
+const bodyParsePatch = require('../index');
+const app = new Koa();
+
+// x-response-time
+
+app.use(async (ctx, next) => {
+    const start = Date.now();
+    await next();
+    const ms = Date.now() - start;
+    ctx.set('X-Response-Time', `${ms}ms`);
+});
+
+// logger
+
+app.use(async (ctx, next) => {
+    const start = Date.now();
+    await next();
+    const ms = Date.now() - start;
+    console.log(`${ctx.method} ${ctx.url} - ${ms}`);
+});
+
+
+app.use(bodyParsePatch({
+    multipart: true,
+    keepExtensions: true,
+    strict: false,
+}));
+app.use(async ctx => {
+    ctx.body = 'Hello World';
+});
+
+app.listen(3000);
